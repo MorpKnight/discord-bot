692ab87814b3cca0b53058d60eda7404a59a6269
MorpKnight <christoffelsihombing@gmail.com>
2023-01-13 05:48:34 +0700

    Fixing Attemp #1

--- cogs/anime.py
+++ cogs/anime.py
@@ -13,29 +13,22 @@ class anime_manga(commands.Cog):
         animePage = AnimeSearch(title)
         result = Anime(animePage.results[0].mal_id)
 
-        # embed = discord.Embed(
-        #     title = f"{result.title}; {result.title_japanese}; {result.title_english}",
-        #     description = f"{result.synopsis}",
-        #     color = discord.Color.random()
-        # )
-        # embed.add_field(name = "Type", value = f"{result.type} - {result.episodes} eps", inline = True)
-        # embed.add_field(name = "Studios", value = f"{result.studios}", inline = True)
-        # embed.add_field(name = "Premiered", value = f"{result.premiered}({result.aired})", inline = True)
-        # embed.add_field(name = "Score/Ranked", value = f"{result.score}/{result.rank}", inline = False)
-        # embed.add_field(name = "Source", value = f"{result.source}", inline = True)
-        # embed.add_field(name = "Status", value = f"{result.status}", inline = True)
-        # embed.add_field(name = "Genres", value = ", ".join(result.genres), inline = False)
-        # embed.set_thumbnail(url = result.image_url)
-        # embed.set_footer(text = f"[Click here]({result.url}) to access via MAL")
+        embed = discord.Embed(
+            title = f"{result.title}; {result.title_japanese}; {result.title_english}",
+            description = f"{result.synopsis}",
+            color = discord.Color.random()
+        )
+        embed.add_field(name = "Type", value = f"{result.type} - {result.episodes} eps", inline = True)
+        embed.add_field(name = "Studios", value = f"{result.studios}", inline = True)
+        embed.add_field(name = "Premiered", value = f"{result.premiered}({result.aired})", inline = True)
+        embed.add_field(name = "Score/Ranked", value = f"{result.score}/{result.rank}", inline = False)
+        embed.add_field(name = "Source", value = f"{result.source}", inline = True)
+        embed.add_field(name = "Status", value = f"{result.status}", inline = True)
+        embed.add_field(name = "Genres", value = ", ".join(result.genres), inline = False)
+        embed.set_thumbnail(url = result.image_url)
+        embed.set_footer(text = f"[Click here]({result.url}) to access via MAL")
 
-        # await interaction.response.send_message(embed=embed)
-
-        message = f"""{result.title}
-{result.image_url}
-{result.title_japanese}
-{result.synopsis}
-{", ".join(result.genres)}"""
-        print(message)
+        await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name='search', description="Search anime or manga in MyAnimeList")
     @app_commands.choices(format = [

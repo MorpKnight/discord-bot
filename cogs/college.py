9157498f9bda43a12971b2e23cfc4166bcc1cca0
MorpKnight <christoffelsihombing@gmail.com>
2023-02-23 17:48:48 +0700

    Remove OPENAI

--- cogs/college.py
+++ cogs/college.py
@@ -1,34 +1,13 @@
-from asyncio import sleep
-from os import getenv
-
 import discord
 import matplotlib.pyplot as plt
 import numpy as np
-import openai
 from discord import app_commands
-from discord.app_commands import Choice
 from discord.ext import commands
-from dotenv import load_dotenv
 from scipy.stats import linregress
-
-load_dotenv()
-openai.api_key = getenv("OPENAI")
 
 class college(commands.Cog):
     def __init__(self, client:discord.Client):
         self.client = client
-
-    def chat(self, machine_type, creativity, prompt):
-        completions = openai.Completion.create(
-            engine = machine_type,
-            prompt = prompt,
-            max_tokens = 1024,
-            n = 1,
-            temperature = creativity
-        )
-
-        message = completions.choices[0].text
-        return message.strip()
 
     @app_commands.command(name='least_square', description="Separate array using space")
     async def leastsquare(self, interaction:discord.Interaction, x_table:str, y_table:str):
@@ -71,21 +50,6 @@ class college(commands.Cog):
 Intercept (a): **{intercept:.4f}**
 Standard error of the estimate (STEYX): **{std_err:.4f}**
 {intercept_message}""", file=discord.File("plot.jpg"))
-
-    @app_commands.command(name="chatgpt", description="Pre-trained Chat GPT")
-    @app_commands.choices(machine_type = [
-        Choice(name="Davinci_002", value="text-davinci-002"),
-        Choice(name="Davinci_003", value="text-davinci-003")
-    ])
-    async def chat_gpt(self, interaction:discord.Interaction, machine_type:Choice[str], creativity:float, msg:str):
-        if machine_type.value == "text-davinci-002":
-            message = self.chat(machine_type.value, creativity, msg)
-        else:
-            message = self.chat(machine_type.value, creativity, msg)
-
-        await sleep(10)
-        await interaction.response.send_message(message)
-    
 
 async def setup(client):
     await client.add_cog(college(client))

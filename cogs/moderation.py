029dcb5a36eb5f02b28060dff7cd76c55fb66fe5
MorpKnight <christoffelsihombing@gmail.com>
2023-03-05 23:44:16 +0700

    Fixing member kick and ban

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -86,6 +86,8 @@ class moderation(commands.Cog):
         await interaction.response.send_message(f"Reloading {extension.value}")
     
     @app_commands.command(name = 'vote', description = "Vote to kick or ban a user")
+    @app_commands.checks.has_permissions(kick_members=True)
+    @app_commands.checks.has_permissions(ban_members=True)
     @app_commands.choices(type = [
         Choice(name = "Kick", value = "kick"),
         Choice(name = "Ban", value = "ban")

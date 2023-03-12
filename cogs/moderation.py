b0cb3bddf1795b5a47251a7b47c7e64556f6e2c0
MorpKnight <christoffelsihombing@gmail.com>
2023-03-12 15:17:22 +0700

    Testing new layout

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,12 +6,12 @@ from asyncio import sleep
 from concurrent.futures import ThreadPoolExecutor
 
 import discord
+import yaml
+from cogs.utility.moderation_button import ban_button, kick_button, VoteButton
 from discord import app_commands
 from discord.app_commands import Choice, checks
-from discord.ext import commands
-
-from cogs.utility.moderation_button import ban_button, kick_button
-
+from discord.ext import commands, tasks
+import datetime
 
 class command_prompt_class():
     def run_cmd(cmd):
@@ -52,6 +52,19 @@ class moderation(commands.Cog):
     def __init__(self, client:discord.Client):
         self.client = client
         self.cmd = command_prompt_class()
+        self.checkQuota.start()
+    
+    @tasks.loop(minutes=1)
+    async def checkQuota(self):
+        with open("config.yml", "r+") as f:
+            data = yaml.safe_load(f)
+            now = datetime.datetime.now()
+            if now.strftime("%H:%M:%S") == "00:00:00":
+                data['quota']['kick'] = 5
+                data['quota']['ban'] = 5
+                f.seek(0)
+                yaml.dump(data, f, default_flow_style=False)
+                f.truncate()
 
     @app_commands.command(name='move', description="Move member to another voice channel")
     @app_commands.checks.has_permissions(move_members=True)
@@ -94,27 +107,33 @@ class moderation(commands.Cog):
     ])
     async def vote_kick_ban(self, interaction:discord.Interaction, type:Choice[str], member:discord.Member, reason:str = None):
         if type.value == "kick":
-                if member.id == 385053392059236353 or member.id == self.client.user.id:
-                    return await interaction.response.send_message("You can't kick me", ephemeral=True)
-                else:
-                    embed = discord.Embed(
-                        title = "Voting to KICK",
-                        description = f"{member.mention} has been put in voting to be kicked from the server.\nReason: {reason}",
-                        color = discord.Color.red()
-                    )
-                    embed.set_footer(text = f'Voting : 0/5')
-                    await interaction.response.send_message(embed=embed, view=kick_button(member))
+            VoteButton(type, member, reason)
         elif type.value == "ban":
-            if member.id == 385053392059236353 or member.id == self.client.user.id:
-                    return await interaction.response.send_message("You can't ban me", ephemeral=True)
-            else:
-                embed = discord.Embed(
-                    title = "Voting to BAN",
-                    description = f"{member.mention} has been put in voting to be banned from the server.\nReason: {reason}",
-                    color = discord.Color.red()
-                )
-                embed.set_footer(text = 'Voting : 0/5')
-                await interaction.response.send_message(embed=embed, view=ban_button(member))
+            VoteButton(type, member, reason)
+        else:
+            await interaction.response.send_message("Invalid type", ephemeral=True)
+        # if type.value == "kick":
+        #         if member.id == 385053392059236353 or member.id == self.client.user.id:
+        #             return await interaction.response.send_message("You can't kick me", ephemeral=True)
+        #         else:
+        #             embed = discord.Embed(
+        #                 title = "Voting to KICK",
+        #                 description = f"{member.mention} has been put in voting to be kicked from the server.\nReason: {reason}",
+        #                 color = discord.Color.red()
+        #             )
+        #             embed.set_footer(text = f'Voting : 0/5')
+        #             await interaction.response.send_message(embed=embed, view=kick_button(member))
+        # elif type.value == "ban":
+        #     if member.id == 385053392059236353 or member.id == self.client.user.id:
+        #             return await interaction.response.send_message("You can't ban me", ephemeral=True)
+        #     else:
+        #         embed = discord.Embed(
+        #             title = "Voting to BAN",
+        #             description = f"{member.mention} has been put in voting to be banned from the server.\nReason: {reason}",
+        #             color = discord.Color.red()
+        #         )
+        #         embed.set_footer(text = 'Voting : 0/5')
+        #         await interaction.response.send_message(embed=embed, view=ban_button(member))
     
     @commands.hybrid_command(name='unban', description = "Unban a user from the server")
     async def _unban(self, ctx, member:discord.Member, *, reason:str=None):
--- cogs/utility/moderation_button.py
+++ cogs/utility/moderation_button.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ui import View
-
+import yaml
 
 class kick_button(View):
     def __init__(self, member:discord.Member):
@@ -8,8 +8,28 @@ class kick_button(View):
         self.member = member
         self.voting = 0
     
+    async def check_quota(self):
+        with open('config.yml', 'r+') as f:
+            config = yaml.safe_load(f)
+            if config['quota']['kick'] <= 0:
+                return False
+            else:
+                config['quota']['kick'] -= 1
+                f.seek(0)
+                yaml.dump(config, f)
+                return True
+    
     async def onVoting(self, number, member, interaction:discord.Interaction):
         if number == 5:
+            if not await self.check_quota():
+                embed = discord.Embed(
+                    title = "Error",
+                    description = "You have reached the quota for kicking member.",
+                    color = discord.Color.red()
+                )
+                await interaction.response.edit_message(embed=embed)
+                return
+
             await member.kick()
             embed = discord.Embed(
                 title = "Kicked",
@@ -80,8 +100,28 @@ class ban_button(View):
         self.member = member
         self.voting = 0
     
+    async def check_quota(self):
+        with open('config.yml', 'r+') as f:
+            config = yaml.safe_load(f)
+            if config['quota']['ban'] <= 0:
+                return False
+            else:
+                config['quota']['ban'] -= 1
+                f.seek(0)
+                yaml.dump(config, f)
+                return True
+    
     async def onVoting(self, number, member, interaction:discord.Interaction):
         if number == 5:
+            if not await self.check_quota():
+                embed = discord.Embed(
+                    title = "Error",
+                    description = "You have reached the quota for kicking member.",
+                    color = discord.Color.red()
+                )
+                await interaction.response.edit_message(embed=embed)
+                return
+            
             await member.ban()
             embed = discord.Embed(
                 title = "Banned",
@@ -129,6 +169,101 @@ class ban_button(View):
         await self.onVoting(self.voting, self.member, interaction)
         await interaction.followup.send(f"{interaction.user} has voted to not ban ", ephemeral = True)
     
+    async def on_timeout(self, interaction:discord.Interaction):
+        embed = discord.Embed(
+            title = 'Timeout',
+            description = "Voting has timed out.",
+            color = discord.Color.red()
+        )
+        for i in self.children:
+            i.disabled = True
+        self.stop()
+        await interaction.response.edit_message(embed=embed, view=self)
+
+class VoteButton(View):
+    def __init__(self, member:discord.Member, type:str):
+        super().__init__(timeout=15)
+        self.member = member
+        self.type = type
+        self.voting = 0
+    
+    async def check_quota(self):
+        with open('config.yml', 'r+') as f:
+            config = yaml.safe_load(f)
+            if config['quota']['kick'] <= 0:
+                return False
+            else:
+                config['quota']['kick'] -= 1
+                f.seek(0)
+                yaml.dump(config, f)
+                return True
+            
+    async def onVoting(self, number, member, interaction:discord.Interaction):
+        if number == 5:
+            if not await self.check_quota():
+                embed = discord.Embed(
+                    title = "Error",
+                    description = "You have reached the quota for kicking member.",
+                    color = discord.Color.red()
+                )
+                await interaction.response.edit_message(embed=embed)
+                return
+            
+            if self.type == 'kick':
+                await member.kick()
+                embed = discord.Embed(
+                    title = "Kicked",
+                    description = f"{member.mention} has been kicked from the server.",
+                    color = discord.Color.red()
+                )
+            elif self.type == 'ban':
+                await member.ban()
+                embed = discord.Embed(
+                    title = "Banned",
+                    description = f"{member.mention} has been banned from the server.",
+                    color = discord.Color.red()
+                )
+            for i in self.children:
+                i.disabled = True
+            await interaction.response.edit_message(embed=embed, view=self)
+        else:
+            embed = discord.Embed(
+                title = "Voting",
+                description = f"{member.mention} has been put in voting to be {self.type} from the server.",
+                color = discord.Color.red()
+            )
+            embed.set_footer(text = f'Voting : {self.voting}/5')
+        try:
+            await interaction.response.edit_message(embed=embed)
+        except:
+            pass
+
+    @discord.ui.button(
+        label = 'Yes',
+        style = discord.ButtonStyle.red
+    )
+    async def voteTrue(self, interaction, button):
+        self.voting += 1
+        await self.onVoting(self.voting, self.member, interaction)
+        if self.voting != 5:
+            try:
+                await interaction.followup.send(f"{interaction.user} has voted to {self.type}.", ephemeral = True)
+            except:
+                pass
+        else:
+            pass
+
+    @discord.ui.button(
+        label = 'No',
+        style = discord.ButtonStyle.green
+    )
+    async def voteFalse(self, interaction, button):
+        self.voting -= 1
+        if self.voting < 0:
+            self.voting = 0
+        await self.onVoting(self.voting, self.member, interaction)
+        await interaction.followup.send(f"{interaction.user} has voted to not {self.type} ", ephemeral = True)
+
     async def on_timeout(self, interaction:discord.Interaction):
         embed = discord.Embed(
             title = 'Timeout',

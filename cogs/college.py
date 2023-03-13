b39595d1a035f6258db322f21864bb132b533012
MorpKnight <christoffelsihombing@gmail.com>
2023-03-13 23:13:16 +0700

    Clear data

--- cogs/college.py
+++ cogs/college.py
@@ -52,8 +52,11 @@ class college(commands.Cog):
 Intercept (a): **{intercept:.4f}**
 Standard error of the estimate (STEYX): **{std_err:.4f}**
 {intercept_message}""", file=discord.File("plot.jpg"))
-        # remove plot.jpg
         os.remove("plot.jpg")
+        x = np.array([])
+        y = np.array([])
+        x_table = ""
+        y_table = ""
         
 
 async def setup(client):
